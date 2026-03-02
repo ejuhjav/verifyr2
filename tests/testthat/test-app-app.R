108bc71a1083fd49a7824ea6c6830ffd8490092f
Juha Javanainen <javanainen@mdpi.com>
2026-03-02 20:00:43 +0100

    test(shiny): 57 implement basic shiny tests for folder and file comparisons

--- tests/testthat/test-app-app.R
+++ tests/testthat/test-app-app.R
@@ -1,13 +1,15 @@
 library(shinytest2)
 
-test_that("{shinytest2} recording: startup", {
-  skip_on_cran()
-
+#' helper method for creating the app for testing
+create_app <- function(name) {
   app_path <- system.file("shiny_examples/app", package = "verifyr2")
-  app <- AppDriver$new(app_path, name = "startup", height = 544, width = 941)
+  app <- AppDriver$new(app_path, name = name, height = 544, width = 941)
+  app
+}
 
-  # test folders and files separately as the app & snapshot contain full
-  # resolved paths that differ depending on user machine.
+#' test folders and files separately as the app & snapshot contain full
+#' resolved paths that differ depending on user machine.
+custom_expect_values <- function(app) {
   expect_true(grepl(
     "base_files",
     app$get_value(input = "folder1")
@@ -34,4 +36,35 @@ test_that("{shinytest2} recording: startup", {
   app$expect_values(
     input = setdiff(names(vals$input), exclude)
   )
+}
+
+test_that("{shinytest2} recording: startup", {
+  skip_on_cran()
+  app <- create_app("startup")
+
+  custom_expect_values(app)
+  app$stop()
+})
+
+test_that("{shinytest2} recording: folder_compare", {
+  skip_on_cran()
+  app <- create_app("folder_compare")
+
+  app$click("go")
+
+  app$wait_for_idle(500)
+  custom_expect_values(app)
+  app$stop()
+})
+
+test_that("{shinytest2} recording: file_compare", {
+  skip_on_cran()
+  app <- create_app("file_compare")
+
+  app$set_inputs(compare_tabs = "tabs_file")
+  app$click("go")
+
+  app$wait_for_idle(500)
+  custom_expect_values(app)
+  app$stop()
 })
