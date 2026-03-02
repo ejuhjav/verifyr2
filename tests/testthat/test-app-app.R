1109b6bc8df201ef107136f90c4b3ead378712bb
Juha Javanainen <javanainen@mdpi.com>
2026-03-02 21:15:42 +0100

    test(shiny): 57 implement basic shiny test for options and advanced test for comment persistence

--- tests/testthat/test-app-app.R
+++ tests/testthat/test-app-app.R
@@ -7,6 +7,19 @@ create_app <- function(name) {
   app
 }
 
+compare_button <- function(index) {
+  button <- paste0(
+    "<button class=\"btn btn-default action-button process_button\" ",
+    "id=\"process_",
+    index,
+    "\" onclick=\"Shiny.setInputValue(&#39;process_row&#39;, ",
+    index,
+    ")\" type=\"button\">\n  ",
+    "<span class=\"action-label\">Compare</span>\n</button>"
+  )
+  button
+}
+
 #' test folders and files separately as the app & snapshot contain full
 #' resolved paths that differ depending on user machine.
 custom_expect_values <- function(app) {
@@ -67,4 +80,106 @@ test_that("{shinytest2} recording: file_compare", {
   app$wait_for_idle(500)
   custom_expect_values(app)
   app$stop()
+})
+
+test_that("{shinytest2} recording: options_reset", {
+  skip_on_cran()
+  app <- create_app("options_reset")
+
+  app$click("configure")
+  app$click("reset_config_modal")
+
+  custom_expect_values(app)
+  app$stop()
+})
+
+test_that("{shinytest2} recording: comment_persistence", {
+  skip_on_cran()
+  app <- create_app("comment_persistence")
+
+  cur <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  all <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+
+  app$click("go")
+  app$set_inputs(summary_out_rows_current = cur, allow_no_input_binding_ = TRUE)
+  app$set_inputs(summary_out_rows_all = all, allow_no_input_binding_ = TRUE)
+  app$set_inputs(
+    summary_out_state = c(
+      1772479993305,
+      0,
+      10,
+      "",
+      TRUE,
+      FALSE,
+      TRUE,
+      c(TRUE, "", TRUE, FALSE, TRUE),
+      c(TRUE, "", TRUE, FALSE, TRUE),
+      c(TRUE, "", TRUE, FALSE, TRUE),
+      c(TRUE, "", TRUE, FALSE, TRUE),
+      c(TRUE, "", TRUE, FALSE, TRUE),
+      c(TRUE, "", TRUE, FALSE, TRUE),
+      c(FALSE, "", TRUE, FALSE, TRUE),
+      c(TRUE, "", TRUE, FALSE, TRUE)
+    ),
+    allow_no_input_binding_ = TRUE
+  )
+
+  # click compare on row 2
+  app$set_inputs(process_row = 2, allow_no_input_binding_ = TRUE)
+  app$set_inputs(
+    summary_out_cell_clicked = c("2", "7", compare_button("2")),
+    allow_no_input_binding_ = TRUE,
+    priority_ = "event"
+  )
+  app$wait_for_idle(500)
+
+  # check that comment field is empty
+  expect_equal(app$get_value(input = "details_out_comments"), "")
+
+  # set and store comment
+  app$set_inputs(details_out_comments = "comment1")
+  app$click("save_comments")
+  expect_equal(app$get_value(input = "details_out_comments"), "comment1")
+
+  app$set_inputs(summary_out_state = c(
+    1772480019284,
+    0,
+    10,
+    "",
+    TRUE,
+    FALSE,
+    TRUE,
+    c(TRUE, "", TRUE, FALSE, TRUE),
+    c(TRUE, "", TRUE, FALSE, TRUE),
+    c(TRUE, "", TRUE, FALSE, TRUE),
+    c(TRUE, "", TRUE, FALSE, TRUE),
+    c(TRUE, "", TRUE, FALSE, TRUE),
+    c(TRUE, "", TRUE, FALSE, TRUE),
+    c(FALSE, "", TRUE, FALSE, TRUE),
+    c(TRUE, "", TRUE, FALSE, TRUE)
+  ), allow_no_input_binding_ = TRUE)
+
+  # click compare on row 1
+  app$set_inputs(process_row = 1, allow_no_input_binding_ = TRUE)
+  app$set_inputs(
+    summary_out_cell_clicked = c("1", "7", compare_button("1")),
+    allow_no_input_binding_ = TRUE,
+    priority_ = "event"
+  )
+  app$wait_for_idle(500)
+
+  # check that comment field is empty
+  expect_equal(app$get_value(input = "details_out_comments"), "")
+
+  # click compare on row 2
+  app$set_inputs(process_row = 2, allow_no_input_binding_ = TRUE)
+  app$set_inputs(
+    summary_out_cell_clicked = c("2", "7", compare_button("2")),
+    allow_no_input_binding_ = TRUE,
+    priority_ = "event"
+  )
+  app$wait_for_idle(500)
+
+  # check that comment field contains value "comment1"
+  expect_equal(app$get_value(input = "details_out_comments"), "comment1")
 })
